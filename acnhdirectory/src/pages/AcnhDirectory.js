2f8c118057717edc26aad666d8010b1c13c6b287
sylhuynh <60867374+sylhuynh@users.noreply.github.com>
2020-05-07 23:07:56 -0700

    updated appi hook up and rendered users names on the page

--- acnhdirectory/src/pages/AcnhDirectory.js
+++ acnhdirectory/src/pages/AcnhDirectory.js
@@ -1,12 +1,31 @@
 import React, { Component } from "react";
 import API from "../utils/API";
 
-function AcnhDirectory() {
+class AcnhDirectory extends Component {
+  state = {
+    users: []
+  }
+  componentDidMount() {
+    API.getUsers().then(res => {
+      this.setState({ users: res.data.results });
+    }).catch(err => {
+      console.log(err);
+    });
+  }
+
+  renderUsers() {
+    return this.state.users.map(user => {
+      return <p>{user.name.first}</p>
+    });
+  }
+  render() {
     return (
       <div>
         <h1>I'm a header!</h1>
+        {this.renderUsers()}
       </div>
     );
   }
-  
-  export default AcnhDirectory;
+}
+
+export default AcnhDirectory;
--- acnhdirectory/src/utils/API.js
+++ acnhdirectory/src/utils/API.js
@@ -1,11 +1,8 @@
 import axios from "axios";
 
 export default {
-    getAllVillagers: () => {
-      return axios.get("http://acnhapi.com/villagers");
-    },
-    getVillagersIcons: (villagerID) => {
-      return axios.get(`http://acnhapi.com/icons/villagers/${villagerID}`);
+    getUsers: () => {
+      return axios.get("https://randomuser.me/api/?results=50&nat=us");
     },
   };
   
