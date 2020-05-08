ddb4eb359153ecf80f919c653b5b50a9964716f5
sylhuynh <60867374+sylhuynh@users.noreply.github.com>
2020-05-07 21:21:11 -0700

    added get req from acnh api in api.js

--- acnhdirectory/src/utils/API.js
+++ acnhdirectory/src/utils/API.js
@@ -1,1 +1,11 @@
 import axios from "axios";
+
+export default {
+    getAllVillagers: () => {
+      return axios.get("http://acnhapi.com/villagers");
+    },
+    getVillagersIcons: (villagerID) => {
+      return axios.get(`http://acnhapi.com/icons/villagers/${villagerID}`);
+    },
+  };
+  
