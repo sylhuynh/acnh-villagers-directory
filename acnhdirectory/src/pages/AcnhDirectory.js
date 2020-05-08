01c343dc768b13a7bdf4475d4d1f611f8201e06d
sylhuynh <60867374+sylhuynh@users.noreply.github.com>
2020-05-07 22:08:13 -0700

    created homepage and imported api and connected to app.js

---
+++ acnhdirectory/src/pages/AcnhDirectory.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+import API from "../utils/API";
+
+function AcnhDirectory() {
+    return (
+      <div>
+        <h1>I'm a header!</h1>
+      </div>
+    );
+  }
+  
+  export default AcnhDirectory;
