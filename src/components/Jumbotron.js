60eadcd0b5ea3c9fee97c868aa03475ef00ea3a2
sylhuynh <60867374+sylhuynh@users.noreply.github.com>
2020-05-09 00:32:44 -0700

    updated jumbotron text

--- src/components/Jumbotron.js
+++ src/components/Jumbotron.js
@@ -4,8 +4,8 @@ function Jumbotron() {
     return (
         <div className="jumbotron jumbotron-fluid">
             <div className="container">
-                <h1 className="display-4">Employee Directory</h1>
-                <p className="lead">Click on the carrots to filter or use the search bar to narrow your results</p>
+                <h1 className="display-4">ATLA Directory</h1>
+                <p className="lead">Click on the carrots to filter or use the search bar to narrow your results by name!</p>
             </div>
         </div>
     );
