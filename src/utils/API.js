4943807b3fcea57634f41c97577b39f0fc17a964
sylhuynh <60867374+sylhuynh@users.noreply.github.com>
2020-11-17 16:51:03 -0800

    change user api route to include all characters from ATLA

--- src/utils/API.js
+++ src/utils/API.js
@@ -2,7 +2,8 @@ import axios from "axios";
 
 export default {
     getUsers: () => {
-      return axios.get("https://last-airbender-api.herokuapp.com/api/v1/characters/random?count=50");
+      return axios.get("https://last-airbender-api.herokuapp.com/api/v1/characters/?perPage=${Num}");
     },
   };
   
+  // https://last-airbender-api.herokuapp.com/api/v1/characters/random?count=50
